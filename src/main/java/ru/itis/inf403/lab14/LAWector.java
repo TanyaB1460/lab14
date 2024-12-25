64dd84c09e0b0d61cea5f6231a5e2c2837ef2454
TanyaB1460 <bobrova1406@gmail.com>
2024-12-25 12:42:35 +0300

    gbgnn

---
+++ src/main/java/ru/itis/inf403/lab14/LAWector.java
@@ -0,0 +1,4 @@
+package ru.itis.inf403.lab14;
+
+public class LAWector plusWector{
+}
---
+++ src/main/java/ru/itis/inf403/lab14/Matrix.java
@@ -0,0 +1,17 @@
+package ru.itis.inf403.lab14;
+
+public class Matrix {
+    private float[][] components;
+
+    public Matrix(float[][] components) {
+
+    }
+
+    public Matrix(LAWector[] rows) {
+
+    }
+
+    public Matrix(int columns, int rows) {
+
+    }
+}
